8f5c5f49ee21b92a390455b6a72b92c1160c5e88
pedrodiasfaria <pedrodiafaria@gmail.com>
2018-05-09 00:46:46 +0100

    some minor fixes on the bonus challenge2

--- test/specs/challenge2.specs.js
+++ test/specs/challenge2.specs.js
@@ -23,55 +23,60 @@ describe('Mindera QA Graduate Challenge 2:', function () {
         expect(finalScore).to.be.equal(expectedScore);
     });
 
-    it('Shoot all the bubbles that show up, lefting always 1 on the screen', function () {
+    it('Shoot all the bubbles that show up, leaving at most 1 bubble on the screen', function () {
 
         var expectedScore = 0;
-        var oneBubble = false;
-
-        /** Once the first bubble appears, we can start clicking */
-        while(!oneBubble){
-            if(challenge2Page.bubbles.length >= 1){
-                oneBubble=true;
-            }
-        }
+        var score = parseInt(challenge2Page.score.getText());
+        var oneBubble = true;
 
         /** Set a timeout for the loop.
          *  We can't test it 'endlessly'
          */
         var startTime = Date.now();
-        var timeOut = 25000;
+        var runTime = 25000;
 
-        do{
-            var bubblesArray = challenge2Page.bubbles;
-
-            /** Every time a new bubble appears, we update the expected score
-             *  If we miss a click and let the window have more than one bubble, the loop will end
-             */
-            if(bubblesArray.length == 1){
-                expectedScore++;               
-            }else if (bubblesArray.length > 1){
-                expectedScore+=bubblesArray.length;
-                oneBubble = false;
-            }                        
-
-            bubblesArray.forEach(function(bubble){
-                bubble.click();
-            }); 
-
-            var score = parseInt(challenge2Page.score.getText());
-            if(!score){
-                score = 0;
-            }
-            expect(score).to.be.equal(expectedScore);
-
-            bubblesArray = challenge2Page.bubbles;
-            if(bubblesArray.length > 1){
-                expectedScore+=bubblesArray.length;
+        /** If there are more than 1 bubble on the screen
+         *  ends the loop and increments the expected score
+         *  equal to the extra bubbles not clicked
+         */
+        function checkMultipleBubbles(arr){
+            if(arr.length > 1){
+                expectedScore+=(arr.length-1);
                 oneBubble = false;
             }
-            console.log("time: " + (Date.now() - startTime));
+        }
 
-        }while(oneBubble && (Date.now() - startTime) < timeOut)
+        function endlessTest(start, timeout){
+            do{
+                var bubblesArray = challenge2Page.bubbles;
+    
+                /** Every time a new bubble appears, we update the expected score
+                 *  If we miss a click and let the window have more than one bubble, the loop will end
+                 */
+                if(bubblesArray.length == 1){
+                    expectedScore++;               
+                }else{
+                    checkMultipleBubbles(bubblesArray);
+                }                        
+    
+                bubblesArray.forEach(function(bubble){
+                    bubble.click();
+                }); 
+
+                score = parseInt(challenge2Page.score.getText());
+                if(!score){
+                    score = 0;
+                }
+                expect(score).to.be.equal(expectedScore);
+    
+                bubblesArray = challenge2Page.bubbles;
+                checkMultipleBubbles(bubblesArray);
+                console.log("time: " + (Date.now() - start));
+    
+            }while(oneBubble && (Date.now() - start) < timeout)
+        }
+
+        endlessTest(startTime, runTime);
 
         /** Confirm one last time */
         expect(score).to.be.equal(expectedScore);
